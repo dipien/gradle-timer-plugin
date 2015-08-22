2c97de05165a196cec25129a1bbf1a4b7998c556
Maxi Rosson <mrosson@despegar.com>
2015-08-22 19:45:36 -0300

    Plugin fixes

--- jdroid-gradle-timer-plugin/src/main/groovy/com/jdroid/gradle/timer/GradleTimerPlugin.groovy
+++ jdroid-gradle-timer-plugin/src/main/groovy/com/jdroid/gradle/timer/GradleTimerPlugin.groovy
@@ -8,9 +8,10 @@ public class GradleTimerPlugin implements Plugin<Project> {
 	@Override
 	void apply(Project project) {
 		project.extensions.create("jdroidGradleTimer", GradleTimerExtension.class, this)
-
-		if (project.jdroidGradleTimer.enableProfiling) {
-			gradle.addListener new TimingsListener(project.jdroidGradleTimer.profilingTag, project.jdroidGradleTimer.url)
+		project.afterEvaluate {
+			if (project.jdroidGradleTimer.enableProfiling) {
+				project.gradle.addListener new TimingsListener(project.jdroidGradleTimer.profilingTag, project.jdroidGradleTimer.url)
+			}
 		}
 	}
 }
