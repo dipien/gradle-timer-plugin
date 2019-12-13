9b09ee1023ce6a8236c668fcdfbf8d4ca96340ad
Maxi Rosson <maxirosson@gmail.com>
2019-12-13 07:58:54 -0300

    Upgraded config

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,11 @@
+object Libs {
+
+    // https://github.com/maxirosson/jdroid-java/releases
+    const val JDROID_JAVA_CORE = "com.jdroidtools:jdroid-java-core:3.0.0"
+
+    // https://github.com/maxirosson/jdroid-java-http/releases
+    const val JDROID_JAVA_HTTP_OKHTTP = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0"
+
+    // https://github.com/junit-team/junit4/releases
+    const val JUNIT = "junit:junit:4.12"
+}
