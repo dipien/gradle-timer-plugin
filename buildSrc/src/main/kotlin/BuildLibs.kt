0e69338c288104e3e72d6570b93891d4eada0658
Maxi Rosson <maxirosson@gmail.com>
2019-12-23 08:28:06 -0300

    Fixes

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -2,4 +2,7 @@ object BuildLibs {
     // https://github.com/maxirosson/jdroid-gradle-plugin/releases
     const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.4.0-SNAPSHOT"
     const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0-SNAPSHOT"
+
+    // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
+    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.2.0"
 }
