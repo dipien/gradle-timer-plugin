b7e568c41604cdd434d27daf71e79b9eb227f459
Maxi Rosson <maxirosson@gmail.com>
2020-08-10 22:06:20 -0300

    Added new property to ignore

--- jdroid-gradle-timer-plugin/src/main/java/com/jdroid/gradle/timer/GradleTimerExtension.kt
+++ jdroid-gradle-timer-plugin/src/main/java/com/jdroid/gradle/timer/GradleTimerExtension.kt
@@ -6,5 +6,5 @@ open class GradleTimerExtension(propertyResolver: PropertyResolver) {
 
     var tag: String = propertyResolver.getRequiredStringProp(::tag.name, "unknown")
     var buildHook: BuildHook? = null
-    var projectPropertiesToIgnore = listOf("android.injected.attribution.file.location")
+    var projectPropertiesToIgnore = listOf("android.injected.attribution.file.location", "org.gradle.kotlin.dsl.provider.cid")
 }
