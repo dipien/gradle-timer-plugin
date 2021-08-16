f3c4f2249b044557dc01bfd77428e9dadd14457d
dipien-ci <dipien2020@gmail.com>
2021-08-16 18:24:37 -0300

    Rename tests packages

--- gradle-timer-plugin/src/test/java/com/dipien/gradle/timer/FakePropertyResolver.kt
+++ gradle-timer-plugin/src/test/java/com/dipien/gradle/timer/FakePropertyResolver.kt
@@ -1,6 +1,6 @@
-package com.jdroid.gradle.timer
+package com.dipien.gradle.timer
 
-import com.jdroid.gradle.timer.common.PropertyResolver
+import com.dipien.gradle.timer.common.PropertyResolver
 
 class FakePropertyResolver(private val properties: Map<String, String> = emptyMap()) : PropertyResolver {
 
